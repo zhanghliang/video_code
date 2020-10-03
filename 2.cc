88208e5aa6ad8cf42d88222f6b86fa6998b6c257
zhanghliang <961133096@qq.com>
2020-10-03 17:51:15 +0800

    新增了根据index萃取元素

--- 2.cc
+++ 2.cc
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-10-01 15:25:09
- * @LastEditTime: 2020-10-03 17:20:16
+ * @LastEditTime: 2020-10-03 17:48:47
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /gtest/2.cc
@@ -75,32 +75,62 @@ namespace imp
         tuple( b value,k ... args ) :Parent( args... ),_val( value ) {}//构造函数 args是包参数的值3,3.14 k是包类型的值int double
         type _val;//值
     };
-
+    //----------------------------------------------------------------
     //get 
     //1.通过 type 通过类型获取值 
     //2.通过index 通过索引获取值
+    //----------------------------------------------------------------
+    //1.通过 type 通过类型获取值 
     template<typename val,typename ...x>
-    class tuple_traits
+    class tuple_traits_type
     {
     public:
         using value = Error;
     };
     template<typename val,typename a,typename ...x>//val 要萃取的类型 a 拆包的type  x参数包 就是在x类型包里面找到val
-    class tuple_traits<val,a,x...> :public tuple_traits<val,x...>
+    class tuple_traits_type<val,a,x...> :public tuple_traits_type<val,x...>
     {
     public:
-        using Parent = tuple_traits<val,x...>;
+        using parent = tuple_traits_type<val,x...>;
         using type = a;
         using element = tuple<a,x...>;//是元组
         //萃取公式
         using cond = Equeal_Type<val,type>;
-        using value = typename Cond_Type<cond::isequal,element,Parent>::value;//如果是同一个类型 就返回element中的value成员就是当前类
+        using value = typename Cond_Type<cond::isequal,element,parent>::value;//如果是同一个类型 就返回element中的value成员就是当前类
         //value是等于的时候element那时候的状态 否则就是Error类 loop无线递归
     };
+    //------------------------------
     template<typename a,typename ...x>
-    constexpr typename tuple_traits<a,x...>::value::type& get( tuple <x...>& val )
+    constexpr typename tuple_traits_type<a,x...>::value::type getbytype( tuple <x...>& val )
     {
-        using type = typename tuple_traits<a,x...>::value;
+        using type = typename tuple_traits_type<a,x...>::value;
+        return static_cast< type& >( val )._val;//val是最下层的子类要向上转型成父类获取当时_val的值
+    }//常量表达式
+    //----------------------------------------------------------------
+    //2.通过index 通过索引获取值
+    template<int index,typename ...x>
+    class tuple_traits_index
+    {
+    public:
+        using value = Error;
+    };
+    template<int index,typename a,typename ...x>
+    class tuple_traits_index<index,a,x...> :public tuple_traits_index<index - 1,x...>
+    {
+    public:
+        using parent = tuple_traits_index<index - 1,x...>;
+        using type = a;
+        using element = tuple<a,x...>;
+
+        //萃取公式
+        using value = typename Cond_Type<index == 0,element,parent>::value;
+
+    };
+    //----------------------------------------------------------------
+    template<int index,typename ...x>
+    constexpr typename tuple_traits_index<index,x...>::value::type getbyindex( tuple <x...>& val )
+    {
+        using type = typename tuple_traits_index<index,x...>::value;
         return static_cast< type& >( val )._val;//val是最下层的子类要向上转型成父类获取当时_val的值
     }//常量表达式
 }
@@ -113,7 +143,9 @@ int main( void )
     // imp::tuple<char,double>& res = static_cast< imp::tuple<char,double>& >( val );
     // std::cout << res._val << std::endl;
 
-    std::cout << imp::get<char>( val ) << std::endl;
+    std::cout << imp::getbytype<char>( val ) << std::endl;
     //如果萃取一个没有的类型 会无线萃取到Error类型 loop没有尽头 报错
+
+    std::cout << imp::getbyindex<0>( val ) << std::endl;
     return 0;
 }
